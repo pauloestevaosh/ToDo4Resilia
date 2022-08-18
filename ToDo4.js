ad3f2331b80670665c0d437e9815afdf0d8b3350
pauloestevaosh <pauloestevaosh@hotmail.com>
2022-08-18 02:04:15 -0300

    commit inicial

---
+++ ToDo4.js
@@ -0,0 +1,25 @@
+
+import chalk from 'chalk';
+import readline from 'readline-sync';
+
+
+
+let i =0, continuar = "S", historias =[];
+
+while (continuar=="S") {
+
+    let quem=readline.question(chalk.bold.cyan('Quem?  :  ')),
+    oQueDeseja =readline.question(chalk.bold.cyan('O que deseja?  :  ')),
+    paraQue =readline.question(chalk.bold.cyan('Para que?  :  '));    
+
+i =(`Como ${chalk.green(quem)}, eu desejo ${chalk.blue(oQueDeseja)}, para ${chalk.magenta(paraQue)}`)
+
+continuar =readline.question('Se deseja escrever mais historias digite  "S" ')
+continuar =continuar.toUpperCase()
+historias.push(i) 
+
+}
+historias.forEach(element => { console.log(element)
+    
+});
+
